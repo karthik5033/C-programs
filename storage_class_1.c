bebfdc83301497d12ae4c8b16e496c91d0cb83f9
karthik5033 <karthikkp5033@gmail.com>
2025-03-29 11:05:51 +0530

    added new code

---
+++ storage_class_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int jumble(int x, int y)
+{
+    x = 2 * x + y;
+    return x;
+}
+int main()
+{
+    int x=2 , y=5;
+    y=jumble(y,x );
+    x=jumble(y,x );
+    printf("%d /n", x);
+    return 0;
+}
+
+// Qn : the value printed by the program is 
+// Ans : 
