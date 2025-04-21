61ce0ecfd8a92b488661a451783461283f75dd84
karthik5033 <karthikkp5033@gmail.com>
2025-04-21 23:13:35 +0530

    Your commit message

---
+++ classroom_programs/ASCII_value.c
@@ -0,0 +1,9 @@
+// Write a program to print the ASCII value of a number.
+#include <stdio.h>
+int main()
+{
+    char a;
+    printf("enter the character : ");
+    scanf("%c", &a);
+    printf("the ASCII value  is %d", a);
+}
