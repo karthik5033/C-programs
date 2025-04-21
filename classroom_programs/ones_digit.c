a909fb42a96f7ffc4bc26a18bb75ebc8ba266118
karthik5033 <karthikkp5033@gmail.com>
2025-04-21 22:55:20 +0530

    Your commit message

--- classroom_programs/ones_digit.c
+++ classroom_programs/ones_digit.c
@@ -1,16 +1,26 @@
-// write a program to print digit at ones place of a number
-#include <stdio.h>
-int main()
-{
-    int n;
-    int p, c;
+// // write a program to print digit at ones place of a number
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     int p, c;
+//     printf("enter the number : ");
+//     scanf("%d", &n);
+//     for(p=1;p<10;p++){
+//         c=n%(10*p);
+//         if(c<10){
+//             break;
+//         }
+//     }
+//     printf("the ones digit is %d", c);
+// }
+
+// code is too complicated , therefore easier method is :
+#include<stdio.h>
+int main(){
+    int n,one;
     printf("enter the number : ");
-    scanf("%d", &n);
-    for(p=1;p<10;p++){
-        c=n%(10*p);
-        if(c<10){
-            break;
-        }
-    }
-    printf("the ones digit is %d", c);
+        scanf("%d", &n);
+        one=n%10;
+        printf("the ones digit is %d", one);
 }
