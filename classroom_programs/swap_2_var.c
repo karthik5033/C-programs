86cf70987865fa82e7c2a31f4e8279f234577b77
karthik5033 <karthikkp5033@gmail.com>
2025-04-21 21:46:08 +0530

    added new code

---
+++ classroom_programs/swap_2_var.c
@@ -0,0 +1,5 @@
+// write a program to swap two variable without the help of third variable
+#include<stdio.h>
+int main(){
+    
+}
---
+++ classroom_programs/swap_3_var.c
@@ -0,0 +1,16 @@
+// write a program to swap two numbers using third variable
+#include <stdio.h>
+int main()
+{
+    int x, y, temp;
+    printf("enter the value of x : ");
+    scanf("%d", &x);
+    printf("enter the value of y : ");
+    scanf("%d", &y);
+    temp = x;
+    x = y;
+    y = temp;
+    printf("value of x after swapping is %d ", x);
+    printf("\nvalue of y after swapping is %d ", y);
+    return 0;
+}
