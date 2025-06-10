d124a11dcc69c58f3eabb3718494da3d16bb8167
karthik5033 <karthikkp5033@gmail.com>
2025-06-10 22:47:07 +0530

    a

--- classroom_programs/string basics/practice.c
+++ classroom_programs/string basics/practice.c
@@ -15,19 +15,3 @@
 //     return 0;
 
 // }
-#include<stdio.h>
-int main(){
-    char str[100];
-    char ch;
-    int i=0;
-    printf("Enter the string : ");
-    ch=getchar();
-    while(ch!='\n'){
-        str[i]=ch;
-        i++;
-        ch = getchar();
-    }
-    str[i]='\0';
-    printf(" the entered string is : %s",str);
-    return 0;
-}
