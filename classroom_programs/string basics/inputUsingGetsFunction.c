368a40c0962ca1bb8489d249f8d31a2eac1e7d26
karthik5033 <karthikkp5033@gmail.com>
2025-06-10 22:45:16 +0530

    a

---
+++ classroom_programs/string basics/inputUsingGetsFunction.c
@@ -0,0 +1,11 @@
+// // write a program to take input of a string and print it using gets function
+// #include <stdio.h>
+// int main()
+// {
+//     char str[100];
+//     printf("Enter the string : ");
+//     gets(str);
+//     printf("The entered string is : %s", &str);
+//     return 0;
+// }
+
---
+++ classroom_programs/string basics/practice.c
@@ -0,0 +1,33 @@
+// #include<stdio.h>
+// int main(){
+//     char str[100];
+//     char ch;
+//     int i=0;
+//     printf("Enter the string : ");
+//     ch=getchar();
+//     while(ch!='\n'){
+//         str[i]=ch;
+//         i++;
+//         ch = getchar();
+//     }
+//     str[i]='\0';
+//     printf(" the entered string is : %s",str);
+//     return 0;
+
+// }
+#include<stdio.h>
+int main(){
+    char str[100];
+    char ch;
+    int i=0;
+    printf("Enter the string : ");
+    ch=getchar();
+    while(ch!='\n'){
+        str[i]=ch;
+        i++;
+        ch = getchar();
+    }
+    str[i]='\0';
+    printf(" the entered string is : %s",str);
+    return 0;
+}
