53ee77e6aa313501ce4bfac33ddb0480558a48c9
karthik5033 <karthikkp5033@gmail.com>
2025-07-24 20:25:07 +0530

    a

---
+++ classroom_programs/c basics/practicewhileLoops.c
@@ -0,0 +1,32 @@
+// // Write a program to calculate sum of numbers between m and n using while loops
+// #include<stdio.h>
+// int main(){
+//     int sum=0,m,n;
+//     printf("Enter the value of m : ");
+//     scanf("%d",&m);
+//     printf("Enter the value of n : ");
+//     scanf("%d", &n);
+//     while(m<=n){
+//         sum+=m;
+//         m++;
+//     }
+//     printf("sum= %d",sum);
+//     return 0;
+// }
+
+
+// // write a program to find the largest of 5 numbers using ternary operators and while loops
+// #include<stdio.h>
+// int main(){
+//     int i=0,large,num;
+//     while(i<5){
+//     printf("Enter the %dth number :",i+1);
+//     scanf("%d",&num);
+//     large=(large>num)?(large):(num);
+//     i++;
+//     }
+//     printf("the largest number among the following is %d",large);
+// }
+
+
+// write a program to count till -1 is encountered,also count the negetives,positives and zeroes encou
